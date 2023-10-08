dcc521b957a2bfe0a519db568740c09946f97172
Sebastian Carman <sebastiancarman.sc@gmail.com>
2023-10-08 16:39:09 -0400

    Create header file

---
+++ GroceryList.h
@@ -0,0 +1,25 @@
+typedef struct {
+    char * item;
+    int * amount;
+} Entry;
+
+/*
+* Adds an item to the list.
+*
+* @param entry The entry to be added to the grocery list.
+*/
+void add(Entry * entry);
+
+/*
+* Removes an item from the list, identified by the name of the item.
+*
+* @param item The name of the item to be removed.
+*/
+void remove(char * item);
+
+/*
+* Prints the list created by the user. This function will be called
+* right before terminating the program after the quit message is
+* input by the user.
+*/
+void print_list();
